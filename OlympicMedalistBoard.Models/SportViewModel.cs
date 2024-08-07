c62a91e9bb45236e38950852372cb56181bd9756
Manpreet <hcmanpreet2005@gmail.com>
2024-08-07 10:33:27 -0500

    CRUD methods for Sports

---
+++ OlympicMedalistBoard.Models/SportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlympicMedalistBoard.Models
+{
+    public class SportViewModel
+    {
+        public int SportID { get; set; }
+        public string SportName { get; set; }
+    }
+}
--- OlympicMedalistBoard/Controllers/SportController.cs
+++ OlympicMedalistBoard/Controllers/SportController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Authorization;**-
 using OlympicMedalistBoard.Models;
 using OlympicMedalistBoard.BLL;
 
@@ -28,63 +28,63 @@ namespace OlympicMedalistBoard.Controllers
         {
             return View(); 
         }
+
         [HttpPost]
-        [HttpPost]
-        public IActionResult Create(Sport sport)
+        public IActionResult Create(SportViewModel sportViewModel)
         {
             if (ModelState.IsValid)
             {
                 Sport newSport = new Sport()
                 {
-                    SportName = sport.SportName,
+                    SportName = sportViewModel.SportName,
                 };
                 _sportService.AddSport(newSport);
                 return RedirectToAction("Index");
             }
-            return View(sport);
+            return View(sportViewModel);
         }
 
         [HttpGet]
         public IActionResult Update(int id)
         {
-            var sportToEdit = _sportService.GetSport(id);
-            if (sportToEdit != null)
+            Sport sport = _sportService.GetSport(id);
+            if (sport != null)
             {
-                return View(sportToEdit);
+                return View(new SportViewModel { SportID = sport.SportID, SportName = sport.SportName });
             }
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult Update(Sport sport)
+        public IActionResult Update(SportViewModel sportViewModel)
         {
             if (ModelState.IsValid)
             {
-                _sportService.UpdateSport(sport);
-                return RedirectToAction("Index");
-            }
-            return View(sport);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            var sport  = _sportService.GetSport(id);
-            
-            if (sport != null)
-            {
-                return View(sport);
+                Sport newSport = new Sport()
+                {
+                    SportID = sportViewModel.SportID,
+                    SportName = sportViewModel.SportName
+                };
+                _sportService.UpdateSport(newSport);
             }
             return RedirectToAction("Index");
         }
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int SportID)
+        public IActionResult Delete(int id)
         {
-            var sport = _sportService.GetSport(SportID);
+            Sport sport  = _sportService.GetSport(id);
+            _sportService.DeleteSport(sport.SportID);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmDelete(int SportID)
+        {
+            Sport sport = _sportService.GetSport(SportID);
 
             if (sport != null)
             {
-                _sportService.DeleteSport(SportID);
+                _sportService.DeleteSport(sport.SportID);
                 return RedirectToAction("Index");
             }
             return RedirectToAction("Index");
